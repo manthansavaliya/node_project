12f342dd57d426dd7002b8f6f6f1a681a414b5f3
manthansavaliya <manthansavaliya784@gmail.com>
2025-03-19 11:36:55 +0530

    fix : some code

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const Post = require('../models/post.js');
 const upload = require('../router/userRoutes.js');
 const User = require('../models/user.js');
@@ -14,10 +15,17 @@ const createPost = async (req, res) => {
             user: [ req.user._id ]
         });
 
-
         await post.save();
 
-        res.status(201).json({ message: "Post Created Successfully." });
+        let data = await User.findByIdAndUpdate(req.user._id, {
+            $push: {
+                post: [ post._id ]
+            }
+        });
+        let userdata = data.post.filter(req.user._id = post._id).push(post._id);
+
+
+        res.status(201).json({ message: "Post Created Successfully.", post });
 
     } catch (error) {
         res.status(400).json({ error: error.message });
